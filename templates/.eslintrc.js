6926931482d6b07b2d16698945aa3c246ee8e991
Robbe Louage <robbelouage@hotmail.Com>
2018-10-06 15:19:48 +0200

    added .eslint.js file
    configuration for eslint

---
+++ templates/.eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": 2018
+    },
+    "rules": {
+        "indent": [
+            "error",
+            "tab"
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "double"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ],
+        "no-console": [
+            "error" , 
+            { allow: ["log"] }
+        ]
+    }
+};
